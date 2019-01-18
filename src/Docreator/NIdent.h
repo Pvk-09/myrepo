1f648697982ce22d542e8d7b07bda4956d253d5f
Nirex0 <Nirex.0@gmail.com>
2019-01-18 14:37:33 +0330

    Add: NIdent Structure

---
+++ src/Docreator/NIdent.h
@@ -0,0 +1,14 @@
+// © 2018 NIREX ALL RIGHTS RESERVED
+
+#ifndef _N_IDENT_H_
+#define _N_IDENT_H_
+
+#include <string>
+
+struct NIdent
+{
+	std::string Type;
+	std::string Name;
+};
+
+#endif // !_N_IDENT_H_
