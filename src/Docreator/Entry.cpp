f1e6367bf7b24ae5bba1b999c510e5c38f0b3a41
Nirex0 <Nirex.0@gmail.com>
2019-01-20 08:33:15 +0330

    Update: No more windows' cringy include

--- src/Docreator/Entry.cpp
+++ src/Docreator/Entry.cpp
@@ -1,11 +1,47 @@
 // © 2018 NIREX ALL RIGHTS RESERVED
 
+#define WIN32_LEAN_AND_MEAN
+#define NOGDICAPMASKS
+#define NOSYSMETRICS
+#define NOMENUS
+#define NOICONS
+#define NOSYSCOMMANDS
+#define NORASTEROPS
+#define OEMRESOURCE
+#define NOATOM
+#define NOCLIPBOARD
+#define NOCOLOR
+#define NOCTLMGR
+#define NODRAWTEXT
+#define NOKERNEL
+#define NONLS
+#define NOMEMMGR
+#define NOMETAFILE
+#define NOMINMAX
+#define NOOPENFILE
+#define NOSCROLL
+#define NOSERVICE
+#define NOSOUND
+#define NOTEXTMETRIC
+#define NOWH
+#define NOCOMM
+#define NOKANJI
+#define NOHELP
+#define NOPROFILER
+#define NODEFERWINDOWPOS
+#define NOMCX
+#define NORPC
+#define NOPROXYSTUB
+#define NOIMAGE
+#define NOTAPE
+#define STRICT
+
+#include <Windows.h>
 #include "NString.h"
 #include "NFile.h"
 #include "NDocData.h"
 #include "NFile.h"
 #include <iostream>
-#include <Windows.h>
 
 std::string Parse(std::string path);
 std::string Process(std::vector<NDocData> data);
