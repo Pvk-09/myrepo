9a7a31cf895360b3fde3718eb431ffc5af4aa86f
Nirex0 <Nirex.0@gmail.com>
2019-01-20 08:24:11 +0330

    Add: Example header file

---
+++ example/Stuff.h
@@ -0,0 +1,39 @@
+
+//-->DOC_CLASS
+//DESC LINE 1
+//DESC LINE 2
+class FOO
+{
+public:
+	//-->DOC_FUNC
+	//DESC LINE 1
+	//INPUT LINE 1
+	//INPUT LINE 2
+	//INPUT LINE 3
+	//OUTPUT
+	void BAR(int x, int y, FOO* pFoo)
+	{
+	}
+	
+	//-->DOC_MEMBER
+	//DESC LINE 1
+	//DESC LINE 2
+	FOO* pNext;
+	
+	//-->DOC_MEMBER
+	//DESC LINE 1
+	//DESC LINE 2
+	//DESC LINE 3
+	FOO* pPrev;
+};
+
+//-->DOC_STRUCT
+//DESC LINE 1
+//DESC LINE 2
+struct StuffCreator
+{
+	
+};
+
+
+
